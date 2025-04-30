52ead21181c424f9850ea3a19933c7ec78faba2d
lucky70707 <luke.dekker@hva.nl>
2025-05-01 00:03:17 +0200

    quick sort added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use leptos::tachys::renderer::dom::Element;
 use rand::prelude::*;
+use web_sys::console::assert;
 
 fn main() {
     // //generate a vec with about a thousand numbers
@@ -43,36 +44,60 @@ fn start() {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-
-
     let mut rng = rand::rng();
     let mut nums: Vec<i16> = (1..1000).collect();
     nums.shuffle(&mut rng);
-    let array: &[i16] = &nums;
-
+    let array: &mut Vec<i16> = &mut nums;
+    dbg!(insertion_sort(array));
 
     visualize_array(array, canvas, context);
 }
 
-fn rgb_string(r: u8, g: u8, b: u8) -> String {
+fn _rgb_string(r: u8, g: u8, b: u8) -> String {
     let output = format!("rgb({},{},{})", r, g, b);
     output
 }
 
-
-
-fn visualize_array(array: &[i16], canvas: web_sys::HtmlCanvasElement, context: web_sys::CanvasRenderingContext2d) {
-   
+fn visualize_array(
+    array: &mut Vec<i16>,
+    canvas: web_sys::HtmlCanvasElement,
+    context: web_sys::CanvasRenderingContext2d,
+) {
     let draw_width: f64 = canvas.width() as f64 / array.len() as f64;
-    let height_amplifier: f64 = 450.0 / array.len() as f64;
+    let height_amplifier: f64 = canvas.height() as f64 / array.len() as f64;
     let y: f64 = canvas.height() as f64;
 
     for (index, integer) in array.iter().enumerate() {
         let x: f64 = index as f64 * draw_width;
         let height = *integer as f64 * height_amplifier;
-
         context.fill_rect(x, y, draw_width, -height);
     }
 }
 
+#[test]
+fn test_insertion_sort(){
+    let mut rng = rand::rng();
+    let mut nums: Vec<i16> = (1..10).collect();
+    let sorted_array: &mut Vec<i16> = &mut(nums.clone());
+    nums.shuffle(&mut rng);
+    let array: &mut Vec<i16> = &mut nums;
+    insertion_sort(array);
+    
+    assert!(array==sorted_array);
 
+}
+
+fn insertion_sort(array: &mut Vec<i16>) {
+    for i in 0..array.len() {
+        let key: i16 = array[i];
+        let mut j: i16 = i as i16- 1;
+
+        while j >= 0 && array[j as usize] > key {  
+
+            array[(j+1) as usize] = array[j as usize];
+            j = j - 1;
+        }
+        
+        array[(j+1) as usize] = key;
+    }
+}
